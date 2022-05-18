1dc0c9c29d599d992c81225100e136e6393449a0
sebastianbonta <sebastian.bonta@gmail.com>
2022-05-18 11:48:57 -0300

    Funciona con la versión nueva the authenticator

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Amplify } from 'aws-amplify';
+
 import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-
-function App() {
+export default function App() {
     return (
-        <div className="App">
-            <header>
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>We now have Auth!</h1>
-            </header>
-            <AmplifySignOut />
-        </div>
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
     );
 }
-
-export default App;
